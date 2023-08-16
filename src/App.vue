4c035362f023b7e2b968564a6ea0974268f454fe
zhanghongwei12 <2259064783@qq.com>
2023-08-16 17:46:14 +0800

    项目起步-scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,14 @@
 <template>
 <!--  一级路由出口-->
   <router-view></router-view>
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+  .test {
+    color: $xtxColor;
 
+  }
 </style>
