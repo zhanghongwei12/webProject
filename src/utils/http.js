e24da1489add6e84746c33eca8d8f883a5030d4f
zhanghongwei12 <2259064783@qq.com>
2023-08-16 16:23:28 +0800

    项目基础配置

---
+++ src/utils/http.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout: 5000
+})
+
+http.interceptors.request.use(config => {
+    return config
+}, e => Promise.reject(e))
+
+http.interceptors.response.use(config => {
+    return config
+}, e => Promise.reject(e))
+
+export default http
