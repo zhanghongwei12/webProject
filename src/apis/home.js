528e52570b112675437a4099db59bf07085e65a2
zhanghongwei12 <2259064783@qq.com>
2023-08-17 00:44:36 +0800

    Home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -6,3 +6,10 @@ export const getBannerAPI = () => {
         url: 'home/banner'
     })
 }
+
+// 获取新鲜好物
+export const getNewAPI = () => {
+    return http({
+        url: '/home/new'
+    })
+}
--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -1,11 +1,70 @@
 <script setup>
+import HomePanel from "@/views/Home/components/HomePanel.vue";
+import { getNewAPI } from "@/apis/home";
+import { ref, onMounted } from "vue";
+
+const newList = ref([])
+const getNew = async () => {
+  const res = await getNewAPI()
+  newList.value = res.data.result
+}
+
+onMounted(() => getNew())
 
 </script>
 
 <template>
-  <div>我是新鲜好物</div>
+  <home-panel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <!-- 下面是插槽主体内容模版 -->
+    <ul class="goods-list">
+      <li v-for="item in newList" :key="item.id">
+        <RouterLink to="/">
+          <img :src="item.picture" alt="" />
+          <p class="name">{{ item.name }}</p>
+          <p class="price">&yen;{{ item.price }}</p>
+        </RouterLink>
+      </li>
+    </ul>
+  </home-panel>
+
 </template>
 
-<style scoped>
 
+<style scoped lang='scss'>
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
+
+  li {
+    width: 306px;
+    height: 406px;
+
+    background: #f0f9f4;
+    transition: all .5s;
+
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .price {
+      color: $priceColor;
+    }
+  }
+}
 </style>
