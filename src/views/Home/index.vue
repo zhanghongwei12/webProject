378fe7c1c8048701e7717122cfeb5356b91d4a13
zhanghongwei12 <2259064783@qq.com>
2023-08-17 00:32:20 +0800

    Home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from "@/views/Home/components/HomeBanner.vue";
 import HomeNew from "@/views/Home/components/HomeNew.vue";
 import HomeHot from "@/views/Home/components/HomeHot.vue";
 import HomeProduct from "@/views/Home/components/HomeProduct.vue";
+import HomePanel from "@/views/Home/components/HomePanel.vue";
 </script>
 
 <template>
@@ -14,6 +15,12 @@ import HomeProduct from "@/views/Home/components/HomeProduct.vue";
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <home-panel title="新鲜好物" sub-title="新鲜好物 值得购买">
+    <div>新鲜毫无的插槽</div>
+  </home-panel>
+  <home-panel title="人气推荐" sub-title="人气推荐 值得购买">
+    <div>人气推荐的插槽</div>
+  </home-panel>
 </template>
 
 <style scoped>
