7fdbd9d840521bb092c0ceac519121714883c6a7
zhanghongwei12 <2259064783@qq.com>
2023-08-19 04:07:23 +0800

    登录-token失效401响应拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from "@/stores/user";
+import router from "@/router";
+
 
 const http = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -21,11 +23,19 @@ http.interceptors.request.use(config => {
 http.interceptors.response.use(config => {
     return config
 }, e => {
-    console.log(e, 'e')
+    // 统一错误提示
     ElMessage({
         type: "warning",
         message: e.response.data.message
     })
+    // 401 token 失效处理
+    if(e.response.status === 401) {
+        // 清除失效 token
+        const userStore = useUserStore()
+        userStore.clearUserInfo()
+        router.push( '/login' )
+
+    }
     Promise.reject(e)
 })
 
