dd98490278fb9038ef78091caa28b467d31ee50e
zhanghongwei12 <2259064783@qq.com>
2023-08-19 02:20:41 +0800

    登录-Pinia用户数据持久化

--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import router from './router'
 
 import { lazyPlugin } from "@/directives";
 import { componentPlugin } from '@/components/index'
+// pinia 持久化插件
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 // 注册懒加载指令
 app.use(lazyPlugin)
