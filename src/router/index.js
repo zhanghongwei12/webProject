5ec65bf8e72806fe9e3a1e0a108b72480f8c5f45
zhanghongwei12 <2259064783@qq.com>
2023-08-18 18:43:22 +0800

    详情页-基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
-import Detail from '@/views/Details/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,9 +26,10 @@ const router = createRouter({
           component: SubCategory
         },
         {
-          path: '/detail/:id',
+          path: 'detail/:id',
           component: Detail
         }
+
       ]
     },
     {
