c53e86bca418c7f6a4241dbfadb7a1673f1bf011
zhanghongwei12 <2259064783@qq.com>
2023-08-18 21:33:51 +0800

    详情页-图片预览组件-props适配图片列表

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -10,7 +10,7 @@ const route = useRoute()
 const getGoods = async () => {
   const res = await getDetail(route.params.id)
   goods.value = res.data.result
-  console.log(res.data.result)
+  // console.log(res.data.result)
 }
 onMounted(() => getGoods())
 </script>
@@ -35,7 +35,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <image-view></image-view>
+              <image-view :imageList="goods.mainPictures"></image-view>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
