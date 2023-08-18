88a75d6f4920422ea269d4b214ac9c52eb8643b5
zhanghongwei12 <2259064783@qq.com>
2023-08-18 22:54:24 +0800

    登录-表单校验实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,25 @@
 <script setup>
+// 表单校验
+import {ref} from "vue";
+
+const form = ref({
+  account: '',
+  password: ''
+})
+
+const rules = {
+  account: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur'},
+    { min: 6, max: 14, message: '密码为6-14字符', trigger: 'blur'}
+  ]
+}
+
+const submit = () => {
+  console.log(form.value)
+}
 
 </script>
 
@@ -24,20 +45,20 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
                      status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="submit">点击登录</el-button>
             </el-form>
           </div>
         </div>
